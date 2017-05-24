0545cc5fe8feb01853c8cb94df1aa7de79399b6d
ansariomar <omar.ansari@cca.edu>
2017-05-23 20:34:42 -0700

    Update ledreflectspotentiometevalue.ino

--- ledreflectspotentiometevalue.ino
+++ ledreflectspotentiometevalue.ino
@@ -1,1 +1,29 @@
 
+  int ledPin = 11;      // LED connected to digital pin 11
+
+int analogPin = A0;   // potentiometer connected to analog pin Ao
+
+int val = 0;         // variable to store the read value
+
+
+
+void setup()
+
+{
+
+  pinMode(ledPin, OUTPUT);   // sets the pin as output
+
+}
+
+
+
+void loop()
+
+{
+
+  val = analogRead(analogPin);   // read the input pin
+
+  analogWrite(ledPin, val / 4);  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
+}
+
+
