a4317a300b6b4b83cac5ce17b6191839eb10bee9
ansariomar <omar.ansari@cca.edu>
2017-06-09 11:44:59 -0700

    Add files via upload

---
+++ Function_2_motor_back_forth_with_push_button_momentary_switch.ino
@@ -0,0 +1,74 @@
+// declare variable pushButton and give it a value of "10" (for pin number 10)
+int pushButton = 10;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+  pinMode (2, OUTPUT);
+  pinMode (4, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (7, OUTPUT);
+  pinMode (8, OUTPUT);
+  pinMode (9, OUTPUT);
+
+  analogWrite (6, 255); // provides power to motor 1
+  analogWrite (9, 255); // provides power to motor 2
+  Serial.begin(9600);
+  // assign the pushbutton pin as an input:
+  pinMode(pushButton, INPUT);
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // read the input pin:
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button:
+  Serial.println(buttonState);
+  // delay in between reads for stability
+  delay(1);
+
+  // create if condition for buttonState >= 1
+  if (buttonState == 1) {
+
+    moveForward (500); // motors powered on and move forward
+
+    stopMotion (500); // motors powered off
+
+    moveBackward (500); // motors powered on and move backward
+
+    stopMotion (500); // motors powered off
+
+
+
+  }
+}
+
+void moveForward (int motionTime) {
+  digitalWrite (2, HIGH);   // turn the motor on and move forward (HIGH is the voltage level)
+  digitalWrite (4, LOW);
+  digitalWrite (7, HIGH);   // turn the motor on and move forward (HIGH is the voltage level)
+  digitalWrite (8, LOW);
+  delay(motionTime);
+}
+
+void stopMotion (int motionTime) {
+  digitalWrite (2, LOW);   // turn the motor off for 0.5 second  (LOW is the voltage level)
+  digitalWrite (4, LOW);
+  digitalWrite (7, LOW);
+  digitalWrite (8, LOW);   // wait for 0.5 second
+  delay(motionTime);
+}
+
+void moveBackward (int motionTime) {
+  digitalWrite (2, LOW);
+  digitalWrite (4, HIGH);  // turn the motor on a move backward (HIGH is the voltage level)
+  digitalWrite (7, LOW);
+  digitalWrite (8, HIGH);  // turn the motor on and move forward (HIGH is the voltage level)
+  delay(motionTime);       // wait for 0.5 second
+
+
+}
+
+
+
